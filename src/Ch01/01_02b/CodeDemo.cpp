0be1286607fe74bdbd949b30082fe269f34bde00
hemassjj <149575723+hemassjj@users.noreply.github.com>
2025-04-02 10:34:33 +0000

    first program

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,8 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include<iostream>
+int main(){
+    std::cout<<"Hi there!"<<std::endl;
+    std::cout<<std::endl<<std::endl;
+}
